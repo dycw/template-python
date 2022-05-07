a4bdb9e53f75c64afec148cc6c59233278221cbd
Derek Wan <d.wan@icloud.com>
2022-05-08 01:35:14 +0800

    Set up nox

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,5 +1,11 @@
+from git.repo import Repo
+
 from dycw_template import __version__
 
 
-def test_main() -> None:
+def test_version() -> None:
     assert isinstance(__version__, str)
+
+
+def test_repo() -> None:
+    assert isinstance(Repo("."), Repo)
