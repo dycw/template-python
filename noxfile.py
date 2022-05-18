45d75d535a5f1e80664cafb1a8969543987c8b0d
Derek Wan <d.wan@icloud.com>
2022-05-18 22:22:47 +0800

    Update to Python 3.9 (#31)

--- noxfile.py
+++ noxfile.py
@@ -10,7 +10,7 @@ options.error_on_missing_interpreters = True
 options.error_on_external_run = True
 
 
-@session(python=["3.7", "3.8", "3.9", "3.10"])
+@session(python=["3.9", "3.10"])
 def test(session: Session) -> None:
     session.install("-r", "requirements-all.txt")
     n = max(round(cpu_count() / 2), 1)
