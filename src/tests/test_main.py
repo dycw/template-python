bbafdac157ac6b7e42e228f1f2832f08e52736d1
Derek Wan <d.wan@icloud.com>
2024-08-30 23:52:44 +0900

    more

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import datetime as dt
 from typing import cast
 
+from utilities.platform import SYSTEM, System
 from utilities.zoneinfo import HONG_KONG, UTC, ensure_time_zone, get_time_zone_name
 from whenever import ZonedDateTime
 
@@ -12,9 +13,14 @@ def test_main() -> None:
     datetime = dt.datetime(1951, 4, 1, 3, tzinfo=HONG_KONG)
     time_zone = ensure_time_zone(cast(dt.timezone, datetime.tzinfo))
     datetime2 = datetime.replace(tzinfo=time_zone)
+    match SYSTEM:
+        case System.windows:
+            use = "Etc/UTC"
+        case _:
+            use = "UTC"
     _ = (
         ZonedDateTime.from_py_datetime(datetime2)
-        .to_tz(get_time_zone_name(UTC))
+        .to_tz(use)
         .to_tz(get_time_zone_name(time_zone))
         .py_datetime()
     )
