407c96e4c607a8406ce7ea1b7545f5524c6587f3
Derek Wan <d.wan@icloud.com>
2024-08-30 23:38:45 +0900

    more

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 
 from utilities.zoneinfo import UTC, get_time_zone_name
 
-from dycw_template import __version__
-
 
 def test_main() -> None:
     assert get_time_zone_name(UTC) == "UTC"
