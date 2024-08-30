9fe93011671cd4143bd0d4af2211efdfb51282d9
Derek Wan <d.wan@icloud.com>
2024-08-30 23:45:02 +0900

    more

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,7 +1,20 @@
 from __future__ import annotations
 
-from utilities.zoneinfo import UTC, get_time_zone_name
+import datetime as dt
+from typing import cast
+
+from utilities.zoneinfo import HONG_KONG, UTC, ensure_time_zone, get_time_zone_name
+from whenever import ZonedDateTime
 
 
 def test_main() -> None:
     assert get_time_zone_name(UTC) == "UTC"
+    datetime = dt.datetime(1951, 4, 1, 3, tzinfo=HONG_KONG)
+    time_zone = ensure_time_zone(cast(dt.timezone, datetime.tzinfo))
+    datetime2 = datetime.replace(tzinfo=time_zone)
+    _ = (
+        ZonedDateTime.from_py_datetime(datetime2)
+        .to_tz(get_time_zone_name(UTC))
+        .to_tz(get_time_zone_name(time_zone))
+        .py_datetime()
+    )
