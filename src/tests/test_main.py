b0c0964d9aa70a550311ff942c63f6f5f817a41e
Derek Wan <d.wan@icloud.com>
2024-08-30 23:36:09 +0900

    try

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -4,4 +4,16 @@ from dycw_template import __version__
 
 
 def test_main() -> None:
-    assert isinstance(__version__, str)
+    from zoneinfo import ZoneInfo
+
+    zi = ZoneInfo("UTC")
+
+    assert isinstance(zi, ZoneInfo)
+
+
+def test_main2() -> None:
+    import datetime as dt
+
+    zi = dt.UTC
+
+    assert isinstance(zi, ZoneInfo)
