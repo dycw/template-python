06ae5eec54ede81ff13c8cc55eb313a10a68ba23
Derek Wan <d.wan@icloud.com>
2025-12-16 22:03:05 +0900

    Use `pre-commit-hook-nitpick` (#159)

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-from dycw_template import __version__
+from template import __version__
 
 
-def test_main() -> None:
-    assert isinstance(__version__, str)
+class TestMain:
+    def test_main(self) -> None:
+        assert isinstance(__version__, str)
